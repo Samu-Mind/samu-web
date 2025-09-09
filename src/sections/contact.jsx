0b1075533cd49d5790b24cf77dd520515a32a644
Samu-Mind <samuelayllonsevilla1@gmail.com>
2025-09-09 09:40:44 +0200

    Changing web icon

---
+++ src/sections/contact.jsx
@@ -0,0 +1,8 @@
+// src/sections/Contact.jsx
+export default function Contact() {
+  return (
+    <section className="h-screen flex items-center justify-center bg-purple-50">
+      <h2 className="text-3xl font-semibold">Hablemos</h2>
+    </section>
+  );
+}
